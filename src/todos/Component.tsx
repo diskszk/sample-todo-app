1db391cc5db4257d421060ee42323371cb236203
diskszk <ibalucky23@gmail.com>
2022-12-27 21:12:58 +0900

    feat: delete, put処理を実装

--- src/todos/Component.tsx
+++ src/todos/Component.tsx
@@ -1,5 +1,11 @@
-import { useQuery } from "react-query";
-import { fetchAllTodos } from "./functions";
+import React from "react";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import {
+  deleteTodo,
+  fetchAllTodos,
+  setCompleteTodo,
+  setNotCompletedTodo,
+} from "./functions";
 import { Todo } from "./types";
 
 export const TodoList: React.FC = () => {
@@ -23,12 +29,66 @@ export const TodoList: React.FC = () => {
 type Props = {
   todo: Todo;
 };
+
 export const TodoItem: React.FC<Props> = ({ todo }) => {
+  const queryClient = useQueryClient();
+
+  const updateCompletedMutate = useMutation(
+    (id: string) => setCompleteTodo(id),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("todos");
+      },
+    }
+  );
+
+  const updateNotCompletedMutate = useMutation(
+    (id: string) => setNotCompletedTodo(id),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("todos");
+      },
+    }
+  );
+
+  const deleteTodoMutate = useMutation((id: string) => deleteTodo(id), {
+    onSuccess: () => {
+      queryClient.invalidateQueries("todos");
+    },
+  });
+
+  const handleChangeCheckbox = async (
+    ev: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const { checked } = ev.target;
+
+    if (checked) {
+      updateCompletedMutate.mutate(todo.id as string);
+      alert("タスクを完了にしました。");
+    } else {
+      updateNotCompletedMutate.mutate(todo.id as string);
+      alert("タスクを未完了にしました。");
+    }
+  };
+
+  const handleClickDeleteButton = async () => {
+    if (!todo.id) {
+      alert("存在しないTodoです");
+      return;
+    }
+    deleteTodoMutate.mutate(todo.id);
+    alert("タスクを削除しました。");
+  };
+
   return (
     <li>
-      <input type="checkbox" />
+      <input
+        type="checkbox"
+        onChange={handleChangeCheckbox}
+        checked={todo.completed}
+      />
       <p>{todo.title}</p>
-      <button>削除</button>
+      <button onClick={handleClickDeleteButton}>削除</button>
     </li>
   );
 };

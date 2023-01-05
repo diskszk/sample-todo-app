3fed3b21160bdbebc949feaae1caeaf1d5ad1826
diskszk <ibalucky23@gmail.com>
2023-01-05 23:05:49 +0900

    feat: serverのcorsを設定する

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,9 +5,11 @@ if (admin.apps.length === 0) {
   admin.initializeApp();
 }
 import * as express from "express";
+import * as cors from "cors";
 import todosRouter from "./todos/controller";
 
 const app = express();
+app.use(cors());
 app.use(todosRouter);
 
 export const api = functions.region("asia-northeast1").https.onRequest(app);

fffcf3924863ffedaa73f6e971a54508d42256b2
diskszk <ibalucky23@gmail.com>
2023-01-07 00:03:43 +0900

    feat: 不要なファイルを削除する

--- packages/client/src/firebase/index.ts
+++
@@ -1,7 +0,0 @@
-import firebase from "firebase/app";
-import { firebaseConfig } from "./firebase-config";
-import "firebase/firestore";
-
-firebase.initializeApp(firebaseConfig);
-
-export const db = firebase.firestore();
